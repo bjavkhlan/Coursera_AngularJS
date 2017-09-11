d862ad6cc9c31ef38ecdc44e46d1a4197f79343c
bjavkhlan <javkhlan96@gmail.com>
2017-09-11 23:56:12 +0800

    Bonus feature

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -13,9 +13,16 @@ function LunchCheckController($scope) {
   $scope.checkIfTooMuch = function() {
     $scope.items = $scope.list.split(",");
   //  console.log($scope.items);
-    if($scope.items.length == 1 && $scope.list=='') $scope.message = "Please enter data first";
-    else if($scope.items.length <= 3) $scope.message = "Enjoy!";
-    else $scope.message = "Too much!";
+    if($scope.items.length == 1 && $scope.list=='') {
+      $scope.message = "Please enter data first";
+      $scope.messageColor = 'red';
+    } else if($scope.items.length <= 3) {
+      $scope.message = "Enjoy!";
+      $scope.messageColor = 'green';
+    } else {
+      $scope.message = "Too much!";
+      $scope.messageColor = 'green';
+    }
   };
 };
 
